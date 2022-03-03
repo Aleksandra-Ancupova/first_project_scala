c06178e0e1112c48a41c9dfe8c9c2f3166f568e3
Aleksandra <alexandra.ancupova@gmal.com>
2022-03-03 22:24:20 +0200

    day3AgeTask

--- src/main/scala/age_calculator_task.scala
+++ src/main/scala/age_calculator_task.scala
@@ -4,10 +4,11 @@ import scala.io.StdIn.readLine
 object age_calculator_task extends App{
   val myName = readLine ("What is your name?")
   println(s"Hi $myName!")
-  val myAge = readLine("How old are you?") .toDouble
+  val myAge = readLine("How old are you?").toInt
   val year = Year.now.getValue
-  val whenHundred = year - myAge + 100
-  val rounded = Math.round(whenHundred)
-  println(s"Cool, you'll be 100 in $rounded")
+  val targetAge = 100
+  val whenHundred = year - myAge + targetAge
+  println(s"Cool, you'll be 100 in $whenHundred")
+
 
 }
---
+++ src/main/scala/booleans_try.scala
@@ -0,0 +1,10 @@
+object booleans_try extends App{
+  println("Hello Booleans!")
+
+  val isLate = true
+  val isSunny = false
+
+  val isMathCorrect = 2*2 ==4
+  println(s"Math 2*2==4 looks fine: $isMathCorrect")
+
+}
