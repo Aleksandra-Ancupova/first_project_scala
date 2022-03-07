0dfae438008d4218b65866bb0a9938a2f8300510
Aleksandra <alexandra.ancupova@gmal.com>
2022-03-07 22:14:01 +0200

    day4 health

--- src/main/scala/temperature_exercise.scala
+++ src/main/scala/temperature_exercise.scala
@@ -13,11 +13,10 @@ object temperature_exercise extends App {
   val name = readLine(s"What's your name?")
   val temp = readLine(s"What's your temperature, $name?").toDouble
   if (temp<35) {
-    println("That is a bit too cold")
-    if (temp>37) {
+    println("That is a bit too cold") }
+   else if (temp>37) {
       println("You have a fever! Please consider contacting a doctor")
     }
-  }
   else println("You are all right!")
 
 }
