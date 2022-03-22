c1c8e0c658d24498af38e22ad9f62a552fd6b548
Aleksandra <alexandra.ancupova@gmal.com>
2022-03-22 20:36:09 +0200

    maps try

---
+++ src/main/scala/mapsExercise.scala
@@ -0,0 +1,26 @@
+import scala.io.StdIn.readLine
+
+object mapsExercise extends App{
+  //TODO ask user to enter a sentence  I live in Liepaja
+  //Split sentence into words using split , you will will have a sequence of words, we did this on Day 8
+  //Generate word length sequence (can use map or yield)
+  //Filter only words of length over 5
+  //print word lengths for every word
+  //print the long words
+
+  //you are allowed to use yield or map/filter
+  val sentence = readLine("Hey, enter any sentence you like!")
+  println(sentence)
+
+  val words = sentence.split(" ")
+  println(words.mkString(","))
+  val wordsSeq = words.toSeq
+  val wordLength = wordsSeq.map(n=>n.length)
+  println(wordLength.mkString(","))  //sequence of word lengths
+
+  val filteredWords = wordLength.filter(n=>n>5)
+  println(filteredWords.mkString(","))
+
+    // val longWords = for (w <- words if wordLength > 5) {
+     // println(s"here are you your Long words: $longWords") }
+}
