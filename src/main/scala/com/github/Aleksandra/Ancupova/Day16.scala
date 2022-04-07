345103dfa64867a9d72d17ef6933790be21595ae
Aleksandra <alexandra.ancupova@gmal.com>
2022-04-07 20:11:28 +0300

    my first package

---
+++ src/main/scala/com/github/Aleksandra/Ancupova/Day16.scala
@@ -0,0 +1,12 @@
+package com.github.Aleksandra.Ancupova
+
+
+case class Laptop(name:String, speed:Double, manufacturer:String)
+
+
+object Day16 extends App {
+  println("Testing some stuff")
+
+  val myLaptop = Laptop("Alex.laptop", 4.5, "Lenovo")
+  println(myLaptop)
+}
