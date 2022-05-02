8ec123c637f30e5063fa076845de7d1ba91efeea
Aleksandra <alexandra.ancupova@gmal.com>
2022-05-02 20:33:38 +0300

    SQL exercise

---
+++ src/resources/db/SQL_Exercises_Day23.sql
@@ -0,0 +1,10 @@
+SELECT * FROM customers
+WHERE Country != "USA";
+
+SELECT * FROM customers
+WHERE Country = "Germany";
+
+SELECT * FROM employees
+WHERE Title = "Sales Support Agent";
+
+SELECT DISTINCT BillingCountry FROM invoices;
