bf241d1bdc482354f63f9669ddbcfb1518f86e14
Aleksandra <alexandra.ancupova@gmal.com>
2022-05-23 16:11:01 +0300

    final assignment update 23.05

--- src/main/scala/com/github/AleksandraAncupova/FinalProject_ToDo_List.scala
+++ src/main/scala/com/github/AleksandraAncupova/FinalProject_ToDo_List.scala
@@ -34,6 +34,8 @@ import scala.io.StdIn.readLine
 
     println("Hi there!\nThis is a Task List Manager")
 
+
+
       /**
        * main loop
        */
@@ -44,22 +46,36 @@ import scala.io.StdIn.readLine
             readLine("\nWhat would you like to do (choose an option please):\n" +
             "(E) enter a new task\n" +
             "(U) update status of existing task\n" +
-            "(R) show remaining tasks\n" +
-            "(F) show finished tasks\n" +
             "(S) print some stats\n" +
-            "(Q) quit\n")
+            "(Q) nothing, just quit\n")
 
-          val input = function.toLowerCase
+              val input = function.toLowerCase
 
-          input match {
-              case "e" => toDo.enterNewTask()
-              case "u" => toDo.updateTaskStatus()
-              case "r" => toDo.showRemainingTasks()
-              case "f" => toDo.showFinishedTasks()
-              case "s" => toDo.printStats()
-              case "q" => toDo.quit()
-              case _ => println("Please choose one of the options above!")
+              input match {
+                case "e" => toDo.enterNewTask()
+                case "u" => toDo.updateTaskStatus()
+                case "s" => statsOption()
+                case "q" => toDo.quit()
+                case _ => println("Please choose one of the options above!")
+              }
             }
+          }
+
+      def statsOption(): Unit = {
+        val input2 = readLine("\nWhich stats would you like to see (choose an option please):\n" +
+          "(R) show remaining tasks\n" +
+          "(F) show finished tasks\n" +
+          "(S) print status summary\n" +
+          "(H) show 5 most urgent tasks\n" +
+          "(Q) nothing, want to quit\n")
+
+        input2 match {
+          case "r" => toDo.showRemainingTasks()
+          case "f" => toDo.showFinishedTasks()
+          case "s" => toDo.printStats()
+          case "h" => toDo.showUrgentTasks()
+          case "q" => toDo.quit()
+          case _ => println("Please choose one of the options above!")
         }
       }
 
